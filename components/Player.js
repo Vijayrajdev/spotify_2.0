53a9c6862770e7dceb8615bd51eab7362178f8da
Vijayraj-wd <vijayrajck@outlook.com>
2022-01-01 12:07:58 +0530

    Player set up

--- components/Player.js
+++ components/Player.js
@@ -1,3 +1,16 @@
+import {
+  HeartIcon,
+  VolumeUpIcon as VolumeDownIcon,
+} from "@heroicons/react/outline";
+import {
+  RewindIcon,
+  SwitchHorizontalIcon,
+  FastForwardIcon,
+  PauseIcon,
+  PlayIcon,
+  ReplyIcon,
+  VolumeUpIcon,
+} from "@heroicons/react/solid";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
@@ -27,6 +40,18 @@ const Player = () => {
     }
   };
 
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body.is_playing) {
+        spotifyApi.pause();
+        setIsplaying(false);
+      } else {
+        spotifyApi.play();
+        setIsplaying(true);
+      }
+    });
+  };
+
   useEffect(() => {
     if (spotifyApi.getAccessToken() && !currentTrackId) {
       fetchCurrentSong();
@@ -35,8 +60,8 @@ const Player = () => {
   }, [currentTrackId, spotifyApi, session]);
 
   return (
-    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white">
-      <div>
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
+      <div className="flex items-center space-x-4">
         <img
           className="hidden md:inline h-10 w-10"
           src={songInfo?.album.images?.[0]?.url}
@@ -46,6 +71,40 @@ const Player = () => {
           <h3>{songInfo?.name}</h3>
           <p>{songInfo?.artists?.[0]?.name}</p>
         </div>
+      </div>
+
+      {/* Center */}
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button hidden md:flex" />
+        <RewindIcon
+          className="button"
+          // onClick={() => spotifyApi.skipToPrevious()} --The Api not working
+        />
+        {isPlaying ? (
+          <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        ) : (
+          <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+        )}
+
+        <FastForwardIcon
+          className="button "
+          //  onClick={() => spotifyApi.skipToNext()} --The Api not working
+        />
+        <ReplyIcon className="button hidden md:flex" />
+      </div>
+
+      {/* right */}
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+        <VolumeDownIcon className="button" />
+        <input
+          className="w-14 md:w-28 "
+          type="range"
+          value={volume}
+          onChange={}
+          min={0}
+          max={100}
+        />
+        <VolumeUpIcon className="button" />
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
 
